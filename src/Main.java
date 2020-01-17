6eaa189c4d6d8899ecaa6f5d73e14ddbbfa2a5a8
mmart19 <59987603+mmart19@users.noreply.github.com>
2020-01-16 19:03:07 -0800

    Added exclamation marks

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("another print line");
-        System.out.println("one last line");
+        System.out.println("hello!!");
+        System.out.println("another print line!!");
+        System.out.println("one last line!!");
     }
 }
